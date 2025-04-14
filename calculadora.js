4169e0669017b4da84cc6863fa4f91c4e90502cc
Miyi-tech <luz.castro.ttc.995@unilibre.edu.co>
2025-04-14 16:19:01 -0500

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,32 @@
+let pantalla = document.getElementById("pantalla");
+let operacion = "";
+
+function agregarNumero(numero){
+    operacion += numero;
+    actualizarPantalla();
+}
+
+function agregarOperador(operador){
+    if( operacion !== "" && !/[\+\-\*\/]$/.test(operacion)){
+        operacion += operador;
+        actualizarPantalla();
+    }   
+}
+
+function calcularResultado() {
+    try{
+        operacion = eval(operacion).toString();
+    } catch (error){
+        operacion = "Error";
+    }
+    actualizarPantalla();
+}
+
+function limpiarPantalla() {
+    operacion = "";
+    actualizarPantalla();
+}
+
+function actualizarPantalla(){
+    pantalla.value = operacion;
+}
